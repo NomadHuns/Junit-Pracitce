ebd5e4b7a48b17b666b30df24b83590ac8872dd8
nomadhuns <kimalss22@gmail.com>
2023-07-05 23:05:32 +0900

    책 등록 Service 메소드 구현

--- src/main/java/com/example/security/service/BookService.java
+++ src/main/java/com/example/security/service/BookService.java
@@ -1,7 +1,31 @@
 package com.example.security.service;
 
+import com.example.security.domain.Book;
+import com.example.security.domain.BookRepository;
+import com.example.security.web.dto.BookResponseDTO;
+import com.example.security.web.dto.BookSaveReqDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    // 책 등록
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookResponseDTO 책등록하기(BookSaveReqDTO requestDTO){
+        Book bookPS = bookRepository.save(requestDTO.toEntity());
+        return new BookResponseDTO().toDTO(bookPS);
+    };
+
+    // 책 목록 조회
+
+    // 책 한건 조회
+
+    // 책 삭제
+
+    // 책 수정
 }
--- src/main/java/com/example/security/web/dto/BookResponseDTO.java
+++ src/main/java/com/example/security/web/dto/BookResponseDTO.java
@@ -1,4 +1,18 @@
 package com.example.security.web.dto;
 
+import com.example.security.domain.Book;
+import lombok.*;
+
+@Getter @NoArgsConstructor
 public class BookResponseDTO {
+    private Long id;
+    private String title;
+    private String author;
+
+    public BookResponseDTO toDTO(Book bookPS) {
+        this.id = bookPS.getId();
+        this.title = bookPS.getTitle();
+        this.author = bookPS.getAuthor();
+        return this;
+    }
 }
