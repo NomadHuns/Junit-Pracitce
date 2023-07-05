124c0a69cf233c1711b6abb297eddd4498e6c0bb
nomadhuns <kimalss22@gmail.com>
2023-07-05 21:31:03 +0900

    책 목록 및 한 건 조회 리파지토리 테스트 완료

--- src/test/java/com/example/security/domain/BookRepositoryTest.java
+++ src/test/java/com/example/security/domain/BookRepositoryTest.java
@@ -1,16 +1,32 @@
 package com.example.security.domain;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DataJpaTest // DB와 관련된 컴포넌트만 메모리에 등
+@DataJpaTest // DB와 관련된 컴포넌트만 메모리에 등록
 public class BookRepositoryTest {
 
     @Autowired
     private BookRepository bookRepository;
+
+//    @BeforeAll // 테스트 시작 전에 한번만 실행
+    @BeforeEach // 각 테스트 시작전에 한번씩 실행
+    public void initBookData() {
+        String title = "junit";
+        String author = "겟인데어";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+    }
 
     // 책 등록
     @Test
@@ -32,8 +48,36 @@ public class BookRepositoryTest {
     }
 
     // 책 목록 조회
+    @Test
+    public void findAll_test() {
+        // given
+        String title = "junit";
+        String author = "겟인데어";
+
+        // when
+        List<Book> bookListPS = bookRepository.findAll();
+
+        // then
+        assertEquals(title, bookListPS.get(0).getTitle());
+        assertEquals(author, bookListPS.get(0).getAuthor());
+
+    }
 
     // 책 한건 조회
+    @Test
+    public void findById_test() {
+        // given
+        String title = "junit";
+        String author = "겟인데어";
+
+        // when
+        Book bookPS = bookRepository.findById(1L).get();
+
+        // then
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+
+    }
 
     // 책 수정
 
